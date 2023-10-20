faabf05eb075ebe447c74acbfe5e325a588e637a
LCS-FS <lcsferreiradesousa@hotmail.com>
2023-10-20 11:37:10 +0100

    admin and users

--- sdle/sdle_app/views.py
+++ sdle/sdle_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest, JsonResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request, 'index.html')
+    #return HttpResponse("Hello, world. You're at the index.")
